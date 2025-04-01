a795af814660d2a886817186a4cff0ab99dc6023
Darwin <d.salcie1@unapec.edu.do>
2025-04-01 01:36:48 -0400

    Entidades Empleado y Horario Consulta Agregadas al DbContext

--- MedicalApp.Infraestructure/Data/AppDbContext.cs
+++ MedicalApp.Infraestructure/Data/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MedicalApp.Domain.Authentication;
+using MedicalApp.Domain.Entities.EmpleadoFolder;
+using MedicalApp.Domain.Entities;
 
 namespace MedicalApp.Infraestructure.Data
 {
@@ -13,6 +15,21 @@ namespace MedicalApp.Infraestructure.Data
         //public DbSet<Doctor> Doctors { get; set; }
         //public DbSet<Patient> Patients { get; set; }
         public DbSet<RefreshToken> RefreshTokens {get; set;}
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<HorarioConsulta> HorarioConsultas { get; set; }
 
+
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<ApplicationUser>().ToTable("Users");
+        //    modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+        //    modelBuilder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+        //    modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+        //    modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+        //    modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        //    modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+        //}
     }
 }
